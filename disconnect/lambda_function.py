6f1a335e307fd050e78b1a9a1b59fb6fdbca5330
wltan <53135010+wltan@users.noreply.github.com>
2020-07-09 22:21:12 +0800

    Clean up queue mappings on disconnect

--- disconnect/lambda_function.py
+++ disconnect/lambda_function.py
@@ -2,6 +2,7 @@ import json
 import boto3
 
 sqs = boto3.client('sqs')
+lmb = boto3.client('lambda')
 
 def lambda_handler(event, context):
     id = event["requestContext"]["connectionId"][:-1]
@@ -13,6 +14,22 @@ def lambda_handler(event, context):
     in_queue_url = sqs.get_queue_url(QueueName=in_queue_name)["QueueUrl"]
     out_queue_url = sqs.get_queue_url(QueueName=out_queue_name)["QueueUrl"]
     src_queue_url = sqs.get_queue_url(QueueName=src_queue_name)["QueueUrl"]
+    
+    out_queue_arn = sqs.get_queue_attributes(
+        QueueUrl=out_queue_url,
+        AttributeNames=["QueueArn"]
+    )["Attributes"]["QueueArn"]
+    
+    out_queue_mappings = lmb.list_event_source_mappings(
+        EventSourceArn=out_queue_arn,
+        FunctionName="runnable-output"
+    )["EventSourceMappings"]
+    
+    try:
+        for mapping in out_queue_mappings:
+            lmb.delete_event_source_mapping(UUID=mapping["UUID"])
+    except ResourceInUseException:
+        print("Mapping still creating or updating")
     
     sqs.delete_queue(QueueUrl=in_queue_url)
     sqs.delete_queue(QueueUrl=out_queue_url)
