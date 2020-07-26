299968da0c10c0eacef817bd814e0bb06ece2edb
wltan <53135010+wltan@users.noreply.github.com>
2020-07-26 21:28:32 +0800

    Box/unbox messages

--- output/lambda_function.py
+++ output/lambda_function.py
@@ -3,9 +3,11 @@ import boto3
 
 api = boto3.client("apigatewaymanagementapi", endpoint_url="https://s4tdw93cwd.execute-api.us-east-1.amazonaws.com/default/")
 
+box_suffix = "-boxed"
+
 def lambda_handler(event, context):
     for message in event["Records"]:
-        output = message["body"]
+        output = message["body"][:-len(box_suffix)]
         attributes = message["messageAttributes"]
         destination = attributes["client"]["stringValue"] + "="
         container_id = attributes["containerId"]["stringValue"]
